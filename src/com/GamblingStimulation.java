10d440273e86f50a0ebcf2f4537a4cee32993d1e
vamshicherala <vamshikrishna199904@gmail.com>
2021-10-20 09:26:37 +0530

    Added:Gambler stake and bet

---
+++ src/com/GamblingStimulation.java
@@ -0,0 +1,15 @@
+package com;
+
+public class GamblingStimulation {
+    public static void main(String[] args) {
+        System.out.println("**Welcome To Gambling**");
+        GamblingStimulation g = new GamblingStimulation();
+        g.gambler();
+    }
+    void gambler(){
+        int stakePerDay = 100;  //Stake of Gambler
+        int bet = 1;            // bet per each game
+        //System.out.println("Stake per day: $"+stakePerDay);
+        //System.out.println("Bet in each game: $"+bet);
+    }
+}
