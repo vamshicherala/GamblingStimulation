e1e2c052d39d0905b9a24b2c1ad8ffffb5b56978
vamshicherala <vamshikrishna199904@gmail.com>
2021-12-07 13:33:59 +0530

    Added:logic to play for 20 days

--- src/com/GamblingStimulation.java
+++ src/com/GamblingStimulation.java
@@ -13,18 +13,41 @@ public class GamblingStimulation {
     int stake = 100;
     int winOrLoss() {
         final int everyDayBet = 1;
+        int die = 0;
+        int afterBet=0;
         Random r1 = new Random();
-        int die = r1.nextInt(2);
-        if (die ==1){
-            stake = stake+everyDayBet;
-        }else {
-            stake = stake-everyDayBet;
+        for (int i=1;i<=20;i++) {
+            stake= afterBet+100;
+            int stakeCheck = stake;
+            System.out.println("bet amount for day "+i+" is: $"+stake);
+            int percent = stake/2;
+            int profit = percent+stake;
+            int loss = percent-stake;
+            while (stake > loss && stake < profit) {
+                die = r1.nextInt(2);
+                if (die == 1) {
+                    afterBet = stake + everyDayBet;
+                } else {
+                    afterBet = stake - everyDayBet;
+                }
+                stake = afterBet;
+            }
+            if (stakeCheck<afterBet){
+                int prof= afterBet-stakeCheck;
+                System.out.println("day "+i+" profit: $"+prof);
+            }else {
+                int los = stakeCheck-afterBet;
+                System.out.println("day "+i+" loss: $"+los);
+            }
         }
+        System.out.println("Profit or loss is: $"+stake);
         return(stake);
     }
     public static void main(String[] args) {
         System.out.println("**Welcome to Gambling Stimulation**");
         GamblingStimulation gamblingStimulation = new GamblingStimulation();
-        System.out.println(gamblingStimulation.winOrLoss());
+        System.out.println("The Gambler starts the day with: $"+gamblingStimulation.stake);
+        System.out.println("The mim amout to bet is: $1");
+        gamblingStimulation.winOrLoss();
     }
 }
