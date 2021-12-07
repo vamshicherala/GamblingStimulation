be4d15760f6ed2130af6d5b2fd6e6f978c6a521d
vamshicherala <vamshikrishna199904@gmail.com>
2021-12-07 11:29:25 +0530

    Added:Win or loss login is added

--- src/com/GamblingStimulation.java
+++ src/com/GamblingStimulation.java
@@ -1,20 +1,30 @@
 package com;
 
+import java.util.Random;
+
 /**
  * @class- main class GamblingStimulator
  * @author - vamshi
  * about- this Program is about a Gambling Stimulation
  */
 
+
 public class GamblingStimulation {
+    int stake = 100;
+    int winOrLoss() {
+        final int everyDayBet = 1;
+        Random r1 = new Random();
+        int die = r1.nextInt(2);
+        if (die ==1){
+            stake = stake+everyDayBet;
+        }else {
+            stake = stake-everyDayBet;
+        }
+        return(stake);
+    }
     public static void main(String[] args) {
         System.out.println("**Welcome to Gambling Stimulation**");
         GamblingStimulation gamblingStimulation = new GamblingStimulation();
-        gamblingStimulation.gambler();
-    }
-    void gambler(){
-        int stake = 100;
-        int everyGameBet = 1;
-        System.out.println("Gambler Starting with stake: $"+stake+" and this every game bet is: $"+everyGameBet);
+        System.out.println(gamblingStimulation.winOrLoss());
     }
 }
